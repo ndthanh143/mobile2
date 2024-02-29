5f1cb9d5f1e423efde65eab5d5f0c1c3654d56e8
Vttien01 <tientienpham01@gmail.com>
2024-02-29 12:11:23 +0700

    Home Page

--- src/navigators/index.jsx
+++ src/navigators/index.jsx
@@ -5,7 +5,7 @@ const Stack = createNativeStackNavigator();
 
 export const Navigators = () => {
   return (
-    <Stack.Navigator initialRouteName="Sign In">
+    <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Sign Up" component={SignUpScreen} />
       <Stack.Screen name="Sign In" component={SignInScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
